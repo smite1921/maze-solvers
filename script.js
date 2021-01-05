daafcc4511b9cc828643e02f9547dacb9957efb9
Smit Patel <smitp505@gmail.com>
2021-01-05 16:28:43 -0500

    Completed binary tree maze gen

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
 const SLIDER = document.getElementsByClassName('slider')[0];
-let LENGTH = SLIDER.value
+const GRID = document.getElementsByClassName('grid-container')[0]
+let LENGTH = parseInt(SLIDER.value)
+
+/**
+ * Makes grid responsive during window resizing
+ */
+window.onresize = () => {
+    const width = window.getComputedStyle(GRID).getPropertyValue('width')
+    GRID.style.height = width 
+}
+
+/**
+ * Callback function for when slider value is changed. It creates a new grid
+ * with the new value of the slider.
+ */
+SLIDER.oninput = () => {
+    LENGTH = parseInt(SLIDER.value)
+    makeGrid(LENGTH)
+}
+
+
+const generateMazeBinary = () => {
+    const boxes = GRID.children
+    
+    // Heads (0) = carve south, (1) Tails = carve East
+    const random = () => Math.floor((Math.random() * 2) + 0)
+
+    for (let i=0; i < boxes.length;i++) {
+        let [row, col] = [Math.floor(i/LENGTH), i % LENGTH]
+        let box = boxes[i]
+        console.log(row, col, i)
+        if (!box.visited) {
+            box.visited = true
+            if ((row == (LENGTH - 1)) && (col == (LENGTH -1))) {
+                continue
+            }
+            else if (row == (LENGTH -1)) {
+                box.classList.add('grid-item-no-right')
+                let boxOnRight = boxes[i+1]
+                boxOnRight.classList.add('grid-item-no-left')
+            }
+            else if (col == (LENGTH - 1)) {
+                box.classList.add('grid-item-no-bottom')
+                let boxBelow = boxes[i+LENGTH]
+                boxBelow.classList.add('grid-item-no-top')
+            }
+            else {
+                let carve = random() == 0 ? true : false
+                box.classList.add(carve ? 'grid-item-no-bottom' : 'grid-item-no-right')
+                let box2 = carve ? boxes[i+LENGTH] : boxes[i+1]
+                box2.classList.add(carve ? 'grid-item-no-top' : 'grid-item-no-left')
+            }
+        }
+    }
+}   
 
 /**
  * Callback function for generate maze form
@@ -7,7 +61,8 @@ let LENGTH = SLIDER.value
 const generateMaze = () => {
     switch (document.getElementById('maze-gen').value) {
         case 'option1':
-            console.log('GM', 1)
+            console.log('Maze Generation - Binary', 1)
+            generateMazeBinary()
             break;
         case 'option2':
             console.log('GM', 2)
@@ -48,31 +103,13 @@ const solveMaze = () => {
 }
 
 /**
- * Makes grid responsive during window resizing
- */
-window.onresize = () => {
-    const grid = document.getElementsByClassName('grid-container')[0]
-    const width = window.getComputedStyle(grid).getPropertyValue('width')
-    grid.style.height = width 
-}
-
-/**
- * Callback function for when slider value is changed. It creates a new grid
- * with the new value of the slider.
- */
-SLIDER.oninput = () => {
-    LENGTH = SLIDER.value
-    makeGrid(LENGTH)
-}
-
-/**
  * Makes a n x n bridge
  * @param {} n
  */
 const makeGrid = (n) => {
-    grid.innerHTML=''
-    grid.style.gridTemplateRows = '1fr '.repeat(n)
-    grid.style.gridTemplateColumns = '1fr '.repeat(n)
+    GRID.innerHTML=''
+    GRID.style.gridTemplateRows = '1fr '.repeat(n)
+    GRID.style.gridTemplateColumns = '1fr '.repeat(n)
 
     const applyCSS = (box, row, col) => {
         box.classList.add('grid-item')
@@ -93,13 +130,13 @@ const makeGrid = (n) => {
     for (let i=0;i<n*n;i++) {
         let [row, col] = [Math.floor(i/n), i % n]
         let gridBox = document.createElement('div')
+        gridBox.visited = false
         applyCSS(gridBox, row, col)
-        grid.appendChild(gridBox)
+        GRID.appendChild(gridBox)
     }
 }
 
-const grid = document.getElementsByClassName('grid-container')[0]
-const width = window.getComputedStyle(grid).getPropertyValue('width')
-grid.style.height = width 
+const width = window.getComputedStyle(GRID).getPropertyValue('width')
+GRID.style.height = width 
 makeGrid(LENGTH)
 
